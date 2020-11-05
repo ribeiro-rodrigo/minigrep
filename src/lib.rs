533830261a9f6deda9cafdb5ff884ec4a182c320
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-11-05 20:31:08 -0200

    Working with Environment Variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,38 @@
 use std::error::Error;
 use std::fs;
+use std::env; 
 
 #[cfg(test)]
 mod tests{
     use super::*; 
 
     #[test]
-    fn one_result(){
+    fn case_sensitive(){
         let query = "duct"; 
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three."; 
+Pick three.
+Duc tape."; 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "rUsT"; 
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me."; 
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents))
     }
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool, 
 }
 
 impl Config {
@@ -29,14 +43,23 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err(); 
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents){
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else{
+        search_case_insensitive(&config.query, &contents)
+    }; 
+
+    for line in results{
         println!("{}", line); 
     }
 
@@ -53,4 +76,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     }
 
     results
-} 
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase(); 
+    let mut results = Vec::new(); 
+
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query){
+            results.push(line); 
+        }
+    }
+
+    results
+}
