c0bcb9ec57fd2999bfb6325f5b000276029ac6dc
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-11-05 20:06:02 -0200

    Developing the Library’s Functionality with Test-Driven Development

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 use std::error::Error;
 use std::fs;
 
+#[cfg(test)]
+mod tests{
+    use super::*; 
+
+    #[test]
+    fn one_result(){
+        let query = "duct"; 
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three."; 
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -21,7 +36,21 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("Contents - {}", contents);
+    for line in search(&config.query, &contents){
+        println!("{}", line); 
+    }
 
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new(); 
+
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+} 
