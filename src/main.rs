4a7977956945df31fb68d895e602f2a94a8be7de
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-11-05 21:06:28 -0200

    Writing Error Messages to Standard Error Instead of Standard Output

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err); 
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
